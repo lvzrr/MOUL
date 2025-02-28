a11fba8673bde4095fcd12def79f76cb448f6aa8
lvzrr <lvzrr@proton.me>
2025-02-28 11:44:37 +0100

    test03 added to interface - fix C02-1

--- tests/test.h
+++ tests/test.h
@@ -1,16 +1,22 @@
 #include "libft.h"
 
-void	ft_putchar(char c);
-void	ft_ft(int *c);
-void	ft_ultimate_ft(int *********c);
-void	ft_swap(int *a, int *b);
-void	ft_div_mod(int a, int b, int *c, int *d);
-void	ft_ultimate_div_mod(int *c, int *d);
-int		ft_strlen(char *str);
-void	ft_rev_int_tab(int *tab, int l);
-void	ft_sort_int_tab(int *tab, int l);
-char	*ft_strcpy(char *dest, char *src);
-char	*ft_strncpy(char *dest, char *src, unsigned int size);
-int		ft_str_is_alpha(char *str);
-int		ft_str_is_numeric(char *str);
-int     ft_str_is_lowercase(char *str);
+void			ft_putchar(char c);
+void			ft_ft(int *c);
+void			ft_ultimate_ft(int *********c);
+void			ft_swap(int *a, int *b);
+void			ft_div_mod(int a, int b, int *c, int *d);
+void			ft_ultimate_div_mod(int *c, int *d);
+int				ft_strlen(char *str);
+void			ft_rev_int_tab(int *tab, int l);
+void			ft_sort_int_tab(int *tab, int l);
+char			*ft_strcpy(char *dest, char *src);
+char			*ft_strncpy(char *dest, char *src, unsigned int size);
+int				ft_str_is_alpha(char *str);
+int				ft_str_is_numeric(char *str);
+int				ft_str_is_lowercase(char *str);
+int				ft_strcmp(char *s1, char *s2);
+int				ft_strncmp(char *s1, char *s2, unsigned int n);
+int				ft_strcat(char *s1, char *s2);
+int				ft_strncat(char *s1, char *s2, unsigned int n);
+char			*ft_strstr(char *haystack, char *needle);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
