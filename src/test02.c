b819e077747d113c8979f840dfbbc532ae04efd7
lvzrr <lvzrr@proton.me>
2025-02-28 15:12:30 +0100

    activated moulinette

--- src/test02.c
+++ src/test02.c
@@ -35,12 +35,12 @@ int libft_test02(void)
     int r;
     size_t nb_tests = sizeof(_02tests) / sizeof(_02tests[0]);
 
-	// r = system("norminette");
-	// if (r != 0)
-	// {
-	// 	libft_printf_err("\e[1;91m[X]\e[0m ERROR: Check the Norm\n");
-	// 	return (1);
-	// }
+	r = system("norminette");
+	if (r != 0)
+	{
+		libft_printf_err("\e[1;91m[X]\e[0m ERROR: Check the Norm\n");
+		return (1);
+	}
     for (size_t i = 0; i < nb_tests; i++)
     {
         char msg[64];
--- src/test03.c
+++ src/test03.c
@@ -21,12 +21,12 @@ int libft_test03(void)
     int r;
     size_t nb_tests = sizeof(_03tests) / sizeof(_03tests[0]);
 
-	// r = system("norminette");
-	// if (r != 0)
-	// {
-	// 	libft_printf_err("\e[1;91m[X]\e[0m ERROR: Check the Norm\n");
-	// 	return (1);
-	// }
+	r = system("norminette");
+	if (r != 0)
+	{
+		libft_printf_err("\e[1;91m[X]\e[0m ERROR: Check the Norm\n");
+		return (1);
+	}
     for (size_t i = 0; i < nb_tests; i++)
     {
         char msg[64];
