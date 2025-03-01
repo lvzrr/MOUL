a96986b5b9d3c3973c3d4e428d7fdc86a2cda386
lvzrr <lvzrr@proton.me>
2025-03-01 19:01:42 +0100

    needfix: c3e4

--- tests/c3e4.c
+++ tests/c3e4.c
@@ -1,37 +1,50 @@
 #include "test.h"
 
-int	main()
-{
-	srand(time(NULL));
-	int	c = 0;
-	while (c < TESTNUM)
-	{
-		char *haystack = (char *)malloc(101 * sizeof(char));
-		int	c2 = 0;
-		int	l =  1 + rand() % 99;
-		while (c2 < l)
- 			haystack[c2++] = ' ' + (rand() % 95);
-		haystack[c2] = 0;
-		int needle_pos = rand() % (l / 2);
-		char *needle = strdup(haystack + needle_pos);
-		char *ft_res = ft_strstr(haystack, needle);
-		if (ft_res == NULL)
-		{
-			libft_printf_err("\n\t\t\e[1;91mTEST FAILED\e[0m:\n\t\t\texpected: %s\n\t\t\tgot: %s\n\t\t\thaystack:  %s\n\n",needle, "(null)", haystack);
+	int main() {
+		srand(time(NULL));
+		int c = 0;
+
+		while (c < TESTNUM) {
+			char *haystack = (char *)malloc(101 * sizeof(char));
+			if (haystack == NULL) {
+				libft_printf_err("Memory allocation failed\n");
+				return (1);
+			}
+
+			int l = (rand() % 47) + 4;
+			int c2 = 0;
+			while (c2 < l)
+				haystack[c2++] = ' ' + rand() % 95;
+			haystack[c2] = '\0';
+
+			int needle_start_pos = rand() % (l - 3);
+			char *needle = haystack + needle_start_pos;
+
+			int needle_len = 3 + rand() % (l - needle_start_pos - 2);
+
+			if (needle_start_pos + needle_len > l) {
+				libft_printf_err("Invalid needle size: %d, haystack length: %d\n", needle_len, l);
+				free(haystack);
+				return (1);
+			}
+
+			if (haystack[l] != '\0') {
+				libft_printf_err("Haystack is not null-terminated properly at position %d\n", l);
+				free(haystack);
+				return (1);
+			}
+
+			char *got = ft_strstr(haystack, needle);
+
+			if (libft_strcmp(needle, got) != 0) {
+				libft_printf_err("\n\t\t\e[1;91mTEST FAILED\e[0m:\n\t\t\texpected: %s\n\t\t\tgot: %s\n\t\t\thaystack:  %s\n\n", needle, got, haystack);
+				free(haystack);
+				return (1);
+			}
+
 			free(haystack);
-			return (1);
-		}
-		if (libft_strcmp(ft_res,needle)!=0)
-		{
-			libft_printf_err("\n\t\t\e[1;91mTEST FAILED\e[0m:\n\t\t\texpected: %s\n\t\t\tgot: %s\n\t\t\thaystack:  %s\n\n",needle, ft_res, haystack);
-			free(haystack);
-			free(needle);
-			return (1);
+			c++;
 		}
 
-		free(haystack);
-		free(needle);
-		c++;
+		return (0);
 	}
-	return (0);
-}
---
+++ tests/c3e5.c
@@ -0,0 +1,45 @@
+#include "test.h"
+
+int	main()
+{
+	srand(time(NULL));
+	int	c = 0;
+	while (c < TESTNUM)
+	{
+		char *src = (char *)calloc(101, sizeof(char));
+		char *dst = (char *)calloc(101, sizeof(char));
+		char *dst2 = (char *)calloc(101, sizeof(char));
+		int l = rand() % 40;
+		int c2 = 0;
+		while (c2 < l)
+			src[c2++] = ' ' + rand() % 95;
+		src[c2] = 0;
+		l = rand() % 49;
+		c2 = 0;
+		while (c2 < l)
+		{
+			dst[c2] = ' ' + rand() % 95;
+			dst2[c2] = dst[c2];
+			c2++;
+		}
+		dst[c2] = 0;
+		dst2[c2] = 0;
+		int n = rand() % 30;
+		unsigned int got = ft_strlcat(dst, src, n);
+		unsigned int ex = libft_strlcat(dst2, src, n);
+		if (ex != got || libft_strcmp(dst, dst2) != 0)
+		{
+			libft_printf_err("\n\t\t\e[1;91mTEST FAILED\e[0m:\n\t\t\texpected: %d\n\t\t\tgot: %d\n\t\t\tsrc:  %s\n\t\t\tdst: %s\n\n",ex, got, src, dst2);
+			free(src);
+			free(dst);
+			free(dst2);
+			return (1);
+		}
+
+		free(src);
+		free(dst);
+		free(dst2);
+		c++;
+	}
+	return (0);
+}
