97a1db978e23ae90a99d6ff7df53a5061dede0b2
lvzrr <lvzrr@proton.me>
2025-03-03 04:04:10 +0100

    up to C05 logs

--- src/libft_iterative_power.c
+++ src/libft_iterative_power.c
@@ -2,14 +2,12 @@
 
 int	libft_iterative_power(int nb, int power)
 {
-	int	i;
+	int	result;
 
 	if (power < 0)
 		return (0);
-	if (power == 0)
-		return (1);
-	i = 1;
-	while (i++ < power)
-		nb *= nb;
-	return (nb);
+	result = 1;
+	while (power-- > 0)
+		result *= nb;
+	return (result);
 }
